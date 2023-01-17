e9675be950dd49a8d9cc58b5af5f26ce74f16f54
geoffrey <gnguyendo@gmail.com>
2023-01-17 14:02:05 -0800

    creates all League Profiles

--- server.js
+++ server.js
@@ -37,73 +37,53 @@ startServer();
 //Riot API Calls
 riotAPIKey = process.env.RIOT_API;
 
+
 leagueQueues = ["RANKED_SOLO_5x5", "RANKED_FLEX_SR", "RANKED_FLEX_TT"];
 leagueTiers = ["DIAMOND", "PLATINUM", "GOLD", "SILVER", "BRONZE", "IRON"];
 leagueDivisions = ["I", "II", "III", "I"];
 
-
-async function createallProfiles(queue, tier, division) {
-    try {    
-        const summonerName = "mynamejefff"
-        const link = `https://na1.api.riotgames.com/lol/league/v4/entries/${queue}/${tier}/${division}?page=1&api_key=${riotAPIKey}`
-        const response = await fetch(link);
-        let data = await response;
-    return data.json();
-    } catch (err) {
-        console.log(err);
+async function updateAllProfilesinRiotQueues(leagueQueues, leagueTiers, leagueDivisions) {
+    for (const queue of leagueQueues) {
+        for (const tier of leagueTiers) {
+            for (const division of leagueDivisions) {
+                try {
+                    const link = `https://na1.api.riotgames.com/lol/league/v4/entries/${queue}/${tier}/${division}?page=1&api_key=${riotAPIKey}`;
+                    const response = await fetch(link);
+                    let data = await response.json();
+                    for (const lolProfile of data) {
+                        const res = await updateByProfile(client, lolProfile.summonerName, 
+                            {
+                                id: lolProfile.summonerName,
+                                leagueId: lolProfile.leagueId,
+                                summonerId: lolProfile.summonerId,
+                                [lolProfile.queueType]: {
+                                    rank: lolProfile.rank,
+                                    tier: lolProfile.tier,
+                                    wins: lolProfile.wins,
+                                    losses: lolProfile.losses,
+                                    leaguePoints: lolProfile.leaguePoints
+                                }
+                            });
+                        // console.log(`${res} documents were updated`)
+                    }
+                } catch (err){
+                    console.log(err);
+                }
+            }
+        }
     }
-};
+    console.log("All Queues Updated")
+}
 
-testQueue = "RANKED_SOLO_5x5"
-testTier = "GOLD"
-testDivision = "I"
-
-
-// async function updateAllAPI(client, ) {
-//     createallProfiles(testQueue, testTier, testDivision).then(data => {
-//         for (const lolProfile of data) {
-//             console.log(lolProfile);
-//         }
-//     })
-// }
 
 async function main () {
     try {
         await client.connect();
-        updateAllAPI(client);
+        updateAllProfilesinRiotQueues(leagueQueues, leagueTiers, leagueDivisions);
     } catch (err) {
         console.log(err)
     }
 };
-
-
-async function updateAllAPI(client) {
-    try {createallProfiles(testQueue, testTier, testDivision).then(async data => {
-        for (const lolProfile of data) {
-            // const queType = {$set: {}};
-            const res = await updateByProfile(client, lolProfile.summonerName, 
-                {
-                    id: lolProfile.summonerName,
-                    leagueId: lolProfile.leagueId,
-                    summonerId: lolProfile.summonerId,
-                    // "files.$.savedId":savedId,
-                    testsadfkjsadfk: {
-                        rank: lolProfile.rank,
-                        tier: lolProfile.tier,
-                        wins: lolProfile.wins,
-                        losses: lolProfile.losses,
-                        leaguePoints: lolProfile.leaguePoints
-                    }
-                
-                });
-            // console.log(`${res} documents were updated`)
-        }
-        console.log("All Profiles created");
-    })}
-    catch (err) {
-        console.log(err);
-    }
-}
 
 
 async function updateByProfile(client, summonerName, updatedProfile) {
